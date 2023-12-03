0f1cf5bf56c1e153c40f8551ea8c4d76727a4a14
Luke McAuley <lukemcauley7@gmail.com>
2023-12-03 11:58:28 +0000

    updated number regex

--- day_one/lib/parser.ex
+++ day_one/lib/parser.ex
@@ -6,14 +6,28 @@ defmodule Parser do
     String.split(text, "\n") |> List.delete_at(-1)
   end
 
+  # First, extract words of numbers, e.g. "five", "four"
+  # Then extrct actual numbers, and the appearance rate
+  # For each of them, determine position in the string, by removing one from appearance rate.
+  # Assign position in string to each number word and number
+  # Grab first and last
+  # Put them together eg 3 and 2 is then 32
+  # return 32 and sum in main
   def read_string(string) do
-    # First, extract words of numbers, e.g. "five", "four"
-    # Then extrct actual numbers, and the appearance rate
-    # For each of them, determine position in the string, by removing one from appearance rate.
-    # Assign position in string to each number word and number
-    # Grab first and last
-    # Put them together eg 3 and 2 is then 32
-    # return 32 and sum in main
+    word_regex = ~r/(one|two|three|four|five|six|seven|eight|nine)/
+    number_regex = ~r/(?<!\d)\b\d\b(?!d)/
+
+    word_instances = Regex.scan(word_regex, string) |> Enum.flat_map(&Enum.uniq/1)
+    number_instances = Regex.scan(number_regex, string)
+
+    word_number_instances = word_instances <> number_instances
+
+    determine_positions(string, word_number_instances)
+  end
+
+  # Determine positions of words and numbers in string, and return first and last
+  def determine_positions(string, instances) do
+    IO.puts(string, instances)
   end
 
   @spec extract_and_combine(String.t()) :: integer | nil
